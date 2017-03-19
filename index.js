7ad4a868d00650b9b0658fd04fabb60863869c4f
Karthik Palaniappan <karth295@uw.edu>
2017-03-18 20:46:27 -0700

    Refactor core of tawk into a statebus widget
    The groups (majority of tawk) live in client/tawk.coffee.
    The main index.html just glues together the widget and a bar to write your name

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-var fs = require('fs')
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-app.use(bodyParser.json());
-var creds = {
-    key: fs.readFileSync('./certs/private-key'),
-    cert: fs.readFileSync('./certs/certificate')
-};
-var http = require('http');
-var httpsServer = require('https').createServer(creds, app);
-
+var express = require('express')
 var bus = require('statebus/server')({
+    full_node: true,
     file_store: false,
-    port: 3004,
     client: function(cbus) {
     },
 });
 
-app.use(express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/../janus-gateway/html/')); // for janus js api
+bus.http.use(express.static(__dirname + '/'))
+bus.http.use(express.static(__dirname + '/../janus-gateway/html/'))
 
-app.get('/:id', function(req, res) {
+function sendIndexHtml(req, res) {
   res.sendFile('index.html', {root: __dirname});
-});
+}
 
-http.createServer(function (req, res) {
-  // Forward to https
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-  res.end();
-}).listen(80);
-
-server = httpsServer.listen(443);
+bus.http.get('/:id', sendIndexHtml);
 
