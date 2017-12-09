c8d606cb9005fa8f895cec8a38ff57c2b28c7671
Michael Toomim <toomim@gmail.com>
2017-12-09 10:52:58 +0000

    cleaned up server

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var express = require('express')
-var bus = require('statebus').serve({
-    file_store: false
-});
+var bus = require('statebus').serve({file_store: false})
+var serve = (x,y) => bus.http.get(x, (r, res) => res.sendFile(__dirname+y))
 
-bus.http.get('/janus.js', (r,res) => {res.sendFile(__dirname+'/janus.js')})
-bus.http.get('/hark.js', (r,res) => {res.sendFile(__dirname+'/node_modules/hark/hark.bundle.js')})
-
-var homepage = (req, res) => res.sendFile(__dirname+'/index.html')
-bus.http.get('/',    homepage)
-bus.http.get('/:id', homepage)
-
-
+serve('/janus.js', '/janus.js')
+serve('/hark.js',  '/node_modules/hark/hark.bundle.js')
+serve('/',         '/index.html')
+serve('/:id',      '/index.html')
